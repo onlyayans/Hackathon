31edc3c7ba9b8a596a579bc8c6bc33636be6a0c9
Ayan Pandey <ayan.pandey@sap.com>
2017-04-29 19:54:52 +0530

    latest changes - going home now
    Change-Id: Iafd1de752167ac93b534960efb03e5d976ce4ea1

--- JavaScriptFiles/StoreDataSourceLinks.js
+++ JavaScriptFiles/StoreDataSourceLinks.js
@@ -10,10 +10,27 @@ function getNewsFeed() {
 	window.location.href = "https://www.google.co.in/search?q=search+fire+in+india&oq=search+fire+in+india&aqs=chrome..69i57j69i64.4370j0j4&sourceid=chrome&ie=UTF-8#q=forest+fire+in+india";
 }
 
-function ready() {
-	$.getJSON("JsonData/twitterResponse.json", function(json) {
-		console.log(json); // this will show the info it in firebug console
+function readPython() {
+	$.ajax({
+		url : "PythonFiles/hello.py",
+		context: document.body
+	}).done(function(o) {
+		console.log(data);
+		console.log(text);
 	});
-	
-	alert("Hello");
 }
+
+function ready() {
+	$.getJSON( "JsonData/twitterResponse.json", function( data ) {
+		  var items = [];
+		  $.each( data, function( key, val ) {
+		    items.push( "<li id='" + key + "'>" + val + "</li>" );
+		  });
+		 
+		  $( "<ul/>", {
+		    "class": "my-new-list",
+		    html: items.join( "" )
+		  }).appendTo( "body" );
+		});
+}
+
