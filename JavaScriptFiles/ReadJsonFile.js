5b7528336685fa0aebf3689fe402f006c0ab85ae
Ayan Pandey <ayan.pandey@sap.com>
2017-04-29 18:56:20 +0530

    last changes
    Change-Id: I771781d5ecf83c762a6906e1217b48593306fd61

--- JavaScriptFiles/ReadJsonFile.js
+++ JavaScriptFiles/ReadJsonFile.js
@@ -1,3 +1,25 @@
-$.getJSON("JsonData/twitterResponse.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
-});
+//function ready () {
+//	$.getJSON("JsonData/twitterResponse.json", function(json) {
+//	    console.log(json); // this will show the info it in firebug console
+//	});
+//}
+
+//jQuery(document).ready(ready);
+//jQuery(document).on('page:load', ready);
+
+//function ready(){
+//    var x = 0;
+//    var twitterResponse=jsonstr;
+//    foreach (i in twitterResponse) {
+//        date = document.getElementById(i.date).innerHTML = twitterResponse.date;
+//        event = document.getElementById(i.event).innerHTML = twitterResponse.event;
+//    }
+//}
+//jQuery(document).ready(ready);
+//jQuery(document).on('page:load', ready);
+
+//$(document).ready(function () {
+//	$.getJSON("JsonData/twitterResponse.json", function(json) {
+//	    console.log(json); // this will show the info it in firebug console
+//	});
+//});
--- JavaScriptFiles/StoreDataSourceLinks.js
+++ JavaScriptFiles/StoreDataSourceLinks.js
@@ -1,4 +1,19 @@
-function msg(){  
+function launchBing() {
 	window.location.href = "https://www.bing.com/";
-// alert("Hello Hackathon");  
+}
+
+function monitorFire() {
+	window.location.href = "FireMonitoring.html";
+}
+
+function getNewsFeed() {
+	window.location.href = "https://www.google.co.in/search?q=search+fire+in+india&oq=search+fire+in+india&aqs=chrome..69i57j69i64.4370j0j4&sourceid=chrome&ie=UTF-8#q=forest+fire+in+india";
+}
+
+function ready() {
+	$.getJSON("JsonData/twitterResponse.json", function(json) {
+		console.log(json); // this will show the info it in firebug console
+	});
+	
+	alert("Hello");
 }
